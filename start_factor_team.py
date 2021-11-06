a1e5a021be51c66f0b1d8233832aaa7bc521c151
kjh1997 <aam41@naver.com>
2021-11-06 16:30:22 +0900

    1106수정본

--- start_factor_team.py
+++ start_factor_team.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import pprint
+import time
 client = MongoClient('203.255.92.141:27017', connect=False)
 ID = client['ID']
 ID_Domestic = ID['Domestic']
@@ -15,62 +16,49 @@ AuthorPapers_A_ID_AND_Papers = list(ntis_client['AuthorPapers'].find({'keyId':51
 scienceon_id = []
 scienceon_dict = {}
 ntis_data = []
-for A_ID_Data in AuthorPapers_A_ID_AND_Papers:
-    # scienceon_id.append(list(ID_Domestic.find({"ntis":A_ID_Data['A_ID']},{"scienceon":1})))
+for A_ID_Data in AuthorPapers_A_ID_AND_Papers[0:20]:
+    scienceon_author = []
+    scienceon_data = []
+    scienceon_data.append({"A_id": A_ID_Data['A_ID']})
+    ntis_paper_cnt = 0
+    paper_cnt = []
+    # keyid 519로 검색했을 경우의 논문
+    for id_paper in A_ID_Data['papers']:
+        
+        
+        ntis_paper_data = list(ntis_rawdata.find({"_id":id_paper},{"koTitle":1,"enTitle":1,"absAbs":1,"effAbs":1,"koKeyword":1,"enKeyword":1,"goalAbs":1}))
+        scienceon_data.append(ntis_paper_data)
+        ntis_paper_cnt += 1 
+    #-----------------------------------------------------    
+    paper_cnt.append(ntis_paper_cnt) # 저자에 대한 ntis 논문 수
+    # keyid 519로 검색했을 경우 나오는 a_id에 대한 scienceon의 id
     author = ID_Domestic.find({"ntis":A_ID_Data['A_ID']},{"scienceon":1,"ntis":1})
-
-#검색한 결과는 ntis의 저자 id 이고 이 결과를 바탕으로 id_domestic 컬렉션에 검색한다. 여기서 나오는 결과는 scienceon의 저자 id 이다.
-# list_paper_title = []
     for id in author:
-        scienceon_data = []
+        
         if id == []:
             continue
         print(id)
-        # print(i)
-        # print(type(i))
         for scienceon_id in id["scienceon"]:
-        # print(j)
+            scienceon_author.append(scienceon_id)
             a = list(scienceon_authorpapers.find({"A_ID":scienceon_id},{"papers":1}))
-        
-            # 여기까지 authorpapers에서 논문 id들을 가져왔다.
+            scienceon_paper_cnt = 0
+       
             for num,scienceon_paper_id in enumerate(a):
-                #  print(k['papers'][0])
                 print(num,list(scienceon_rawdata.find({"_id":scienceon_paper_id['papers'][0]},{"title":1,"abstract":1,"english_title":1,"qryKeyword":1})))
                 scienceon_data.append(list(scienceon_rawdata.find({"_id":scienceon_paper_id['papers'][0]},{"title":1,"abstract":1,"english_title":1,"qryKeyword":1})))
+                scienceon_paper_cnt += 1
+            paper_cnt.append(scienceon_paper_cnt)  # 각 scienceon id 별 논문 수
+    
     
     print(id['ntis'], "한사람 끝")
+    scienceon_data.append({"Scienceon_id":scienceon_author})
+    scienceon_data.append({"paper_cnt":paper_cnt})
     scienceon_dict[id['ntis']] = scienceon_data
     pprint.pprint(scienceon_dict)
-    # for id in scienceon_id:
-    #     if id == []:
-    #         continue
-    #     # print(i)
-    #     # print(type(i))
-    #     for scienceon_id in id[0]["scienceon"]:
-    #     # print(j)
-    #         a = list(scienceon_authorpapers.find({"A_ID":scienceon_id},{"papers":1}))
-        
-    #         # 여기까지 authorpapers에서 논문 id들을 가져왔다.
-    #         for num,scienceon_paper_id in enumerate(a):
-    #             #  print(k['papers'][0])
-    #             print(num,list(scienceon_rawdata.find({"_id":scienceon_paper_id['papers'][0]},{"title":1,"abstract":1,"english_title":1,"qryKeyword":1})))
-    #             scienceon_data.append(list(scienceon_rawdata.find({"_id":scienceon_paper_id['papers'][0]},{"title":1,"abstract":1,"english_title":1,"qryKeyword":1})))
+    time.sleep(0.5)
+    
+    
 print(scienceon_data)
 # -----------------------------------------------------------ntis 코드 ---------------------------------------------------------------
-for ntis_author_paper_id in AuthorPapers_A_ID_AND_Papers[0:20]:
-    for id in ntis_author_paper_id['papers']:
-        print(list(ntis_rawdata.find({"_id":id},{"koTitle":1,"enTitle":1,"absAbs":1,"effAbs":1,"koKeyword":1,"enKeyword":1,"goalAbs":1})))
-        ntis_data.append(list(ntis_rawdata.find({"_id":id},{"mngId":1,"koTitle":1,"enTitle":1,"absAbs":1,"effAbs":1,"koKeyword":1,"enKeyword":1,"goalAbs":1})))
-       # print(list(ntis_rawdata.find({"_id":ObjectId(j['_id'])})))
-adata = {}
 
-for i in ntis_data:
-    adata[i[0]["mngId"]] = []
-print(adata)   
-for i in ntis_data:
-    for j in adata:
-        if j == i[0]["mngId"]:
-            adata[j].append(i)
-print("이거 실행 됐나?                                     ", adata)   
-pprint.pprint(adata)
 
